567b2d7ec1777dac14662ea2582a5cbd8125bc5d
Maurdeer <josephmseo@gmail.com>
2023-09-22 12:40:02 -0400

    Create character selection
    Create character selection
    Add sprites into Resource Manager

--- app/src/main/java/com/example/a2340c_team45/InitialActivity.java
+++ app/src/main/java/com/example/a2340c_team45/InitialActivity.java
@@ -2,40 +2,53 @@ package com.example.a2340c_team45;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 
 public class InitialActivity extends AppCompatActivity {
+
+    ImageButton spr1Button, spr2Button, spr3Button;
+    EditText name;
+    Player player = new Player();
+    Bitmap playerImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+        name = findViewById(R.id.name);
+        spr1Button = findViewById(R.id.spr1Button);
+        spr2Button = findViewById(R.id.spr2Button);
+        spr3Button = findViewById(R.id.spr3Button);
+
+        spr1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerImagePath = ((BitmapDrawable)spr1Button.getDrawable()).getBitmap();
+            }
+        });
+
+        spr2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerImagePath = ((BitmapDrawable)spr2Button.getDrawable()).getBitmap();
+            }
+        });
+
+        spr3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerImagePath = ((BitmapDrawable)spr3Button.getDrawable()).getBitmap();
+            }
+        });
+
+
     }
 
-    public static class MainActivity extends AppCompatActivity {
-
-        Button startBtn;
-        Button exitBtn;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.start_screen);
-            startBtn = findViewById(R.id.startButton);
-            exitBtn = findViewById(R.id.exitButton);
-
-
-    //        startBtn.setOnClickListener(new View.OnClickListener() {
-    //            @Override
-    //            public void onClick(View view) {
-    //                Intent intent = new Intent(getApplicationContext(), InitialActivity.class);
-    //                startActivities(new Intent[]{intent});
-    //            }
-    //        });
-
-    //        exitBtn.setOnClickListener(v -> {
-    //            finishAffinity();
-    //        });
-        }
-    }
 }
---
+++ app/src/main/java/com/example/a2340c_team45/Player.java
@@ -0,0 +1,11 @@
+package com.example.a2340c_team45;
+
+import android.graphics.Bitmap;
+
+public class Player {
+    int health;
+    String name;
+    int difficulty;
+    Bitmap playerImage;
+
+}
