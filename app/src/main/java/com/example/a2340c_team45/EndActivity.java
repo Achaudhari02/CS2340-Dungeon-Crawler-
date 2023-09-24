05f8ff8ba6d6a4126cb430ac66805cf9a2aa730b
riadalvi <riadalvi12@gmail.com>
2023-09-23 22:03:42 -0400

    Added end screen

---
+++ app/src/main/java/com/example/a2340c_team45/EndActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2340c_team45;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+    }
+}
