9ac080424821eb47e931f3108ea2a34b95cb9d73
Achaudhari02 <ashutoshc018@gmail.com>
2023-09-24 03:01:19 -0400

    created the end game screen and added the botton to move on the end screen from the game screen

--- app/src/main/java/com/example/a2340c_team45/GameActivity.java
+++ app/src/main/java/com/example/a2340c_team45/GameActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ public class GameActivity extends AppCompatActivity {
     TextView reciever_msg_name;
     TextView reciever_mag_diff;
     TextView starting_hp;
+    Button endButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,21 @@ public class GameActivity extends AppCompatActivity {
         reciever_msg_name = findViewById(R.id.textView2);
         reciever_mag_diff = findViewById(R.id.sel_diff_id);
         starting_hp = findViewById(R.id.starting_hp_id);
+        endButton = findViewById(R.id.endButton);
         int diff = getIntent().getIntExtra("diff", 1);
         String diff_str = String.valueOf(diff);
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         reciever_msg_name.setText(name);
         reciever_mag_diff.setText(diff_str);
+
+        endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EndGame.class);
+                startActivities(new Intent[]{intent});
+            }
+        });
         if(diff == 1){
 
             starting_hp.setText("100");
