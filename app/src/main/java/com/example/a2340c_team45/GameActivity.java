16184d225a796405ddbf8eec54244592be0fba1c
Achaudhari02 <ashutoshc018@gmail.com>
2023-09-24 02:49:06 -0400

    game screen now shows different starting hp based on the difficulty selected by the player

--- app/src/main/java/com/example/a2340c_team45/GameActivity.java
+++ app/src/main/java/com/example/a2340c_team45/GameActivity.java
@@ -10,9 +10,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class GameActivity extends AppCompatActivity {
     TextView reciever_msg_name;
     TextView reciever_mag_diff;
+    TextView starting_hp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,23 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.game_activity);
         reciever_msg_name = findViewById(R.id.textView2);
         reciever_mag_diff = findViewById(R.id.sel_diff_id);
+        starting_hp = findViewById(R.id.starting_hp_id);
         int diff = getIntent().getIntExtra("diff", 1);
         String diff_str = String.valueOf(diff);
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         reciever_msg_name.setText(name);
         reciever_mag_diff.setText(diff_str);
+        if(diff == 1){
+
+            starting_hp.setText("100");
+        }else if(diff == 2){
+
+            starting_hp.setText("75");
+        }else{
+            starting_hp.setText("50");
+        }
+
 
 
 
