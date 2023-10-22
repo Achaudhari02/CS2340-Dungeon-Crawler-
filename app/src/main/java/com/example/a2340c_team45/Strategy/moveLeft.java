1f54c2d408cddc3ab2187987e09e1f9df866628f
Jake Wang <jakewang2678@gmail.com>
2023-10-21 21:27:34 -0400

    Movement goes faster

--- app/src/main/java/com/example/a2340c_team45/Strategy/moveLeft.java
+++ app/src/main/java/com/example/a2340c_team45/Strategy/moveLeft.java
@@ -5,7 +5,7 @@ import com.example.a2340c_team45.models.Player;
 public class moveLeft implements MovementStrat {
     public void move(Player player) {
         if (ableMove(player)) {
-            player.setX(player.getX() - 5);
+            player.setX(player.getX() - 10);
         }
     }
     public boolean ableMove(Player player) {
