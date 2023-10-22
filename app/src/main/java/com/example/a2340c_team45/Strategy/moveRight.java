51199f91f23dc65e6c96a044977bf64331096887
Jake <jwang3335@gatech.edu>
2023-10-21 21:15:35 -0400

    Fix player movement direction

--- app/src/main/java/com/example/a2340c_team45/Strategy/moveRight.java
+++ app/src/main/java/com/example/a2340c_team45/Strategy/moveRight.java
@@ -5,7 +5,7 @@ import com.example.a2340c_team45.models.Player;
 public class moveRight implements MovementStrat {
     public void move(Player player) {
         if (ableMove(player)) {
-            player.setX(player.getX() + 50);
+            player.setX(player.getX() - 5);
         }
     }
     public boolean ableMove(Player player) {
--- app/src/main/java/com/example/a2340c_team45/Strategy/moveUp.java
+++ app/src/main/java/com/example/a2340c_team45/Strategy/moveUp.java
@@ -5,7 +5,7 @@ import com.example.a2340c_team45.models.Player;
 public class moveUp implements MovementStrat {
     public void move(Player player) {
         if (ableMove(player)) {
-            player.setY(player.getY() + 50);
+            player.setY(player.getY() - 5);
         }
     }
     public boolean ableMove(Player player) {
