4d1aa112fbf08073f94d36e94927eebf480d9ff4
Jake <jwang3335@gatech.edu>
2023-11-12 17:35:05 -0500

    Unit Test for Enemy Speed

---
+++ app/src/test/java/com/example/a2340c_team45/EnemySpeedTest.java
@@ -0,0 +1,20 @@
+package com.example.a2340c_team45;
+import static org.junit.Assert.assertEquals;
+
+import com.example.a2340c_team45.models.Enemy;
+import com.example.a2340c_team45.models.EnemyFactory;
+
+import org.junit.Test;
+public class EnemySpeedTest {
+    @Test
+    public void testEnemy1() {
+        EnemyFactory factory = new EnemyFactory();
+        Enemy enemy = factory.getEnemy("enemy1");
+        assertEquals(5, enemy.getSpeed());
+    }
+    public void testEnemy2() {
+        EnemyFactory factory = new EnemyFactory();
+        Enemy enemy = factory.getEnemy("enemy2");
+        assertEquals(10, enemy.getSpeed());
+    }
+}
