1c7f6f73a2006100d62c068c3ff9ea6035ed2f9f
Jake <jwang3335@gatech.edu>
2023-11-12 12:34:55 -0500

    Fix strategy issues

--- app/src/main/java/com/example/a2340c_team45/Strategy/MoveUpDown.java
+++ app/src/main/java/com/example/a2340c_team45/Strategy/MoveUpDown.java
@@ -1,24 +1,22 @@
 package com.example.a2340c_team45.Strategy;
 
+import com.example.a2340c_team45.Observer.EnemySubscriber;
 import com.example.a2340c_team45.models.Enemy;
 
-public class MoveUpDown {
-    public void moveUp(Enemy enemy) {
-        if (ableMoveUp(enemy)) {
-            enemy.setY(enemy.getY() - enemy.getSpeed());
+public class MoveUpDown implements EnemySubscriber, EnemyMovementStrat{
+    public void move(Enemy enemy) {
+        if (ableMove(enemy)) {
+            enemy.setY(enemy.getY() - enemy.getSpeed()); // move up
+        } else {
+            enemy.setY(enemy.getY() + enemy.getSpeed()); // move down
         }
     }
-    public boolean ableMoveUp(Enemy enemy) {
+    public boolean ableMove(Enemy enemy) {
 
         if (enemy.getY() < 0) {
             return false;
         }
         return true;
-    }
-    public void moveDown(Enemy enemy) {
-        if (ableMoveDown(enemy)) {
-            enemy.setY(enemy.getY() + enemy.getSpeed());
-        }
     }
     public boolean ableMoveDown(Enemy enemy) {
 
@@ -26,5 +24,8 @@ public class MoveUpDown {
             return false;
         }
         return true;
+    }
+    public void updateEnemy(Enemy enemy) {
+        enemy.move();
     }
 }
