18eda0f1110fbc84879c1a73f2e796320a535db3
udukle <umadukle@gmail.com>
2023-11-26 20:35:23 -0600

    decorator pattern

---
+++ app/src/main/java/com/example/a2340c_team45/models/PlayerDecorator.java
@@ -0,0 +1,23 @@
+package com.example.a2340c_team45.models;
+
+public abstract class PlayerDecorator {
+    private int x;
+    private int y;
+
+    private boolean isCollected = false;
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+
+}
