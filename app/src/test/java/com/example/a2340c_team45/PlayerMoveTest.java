e370078922fca6572cf698b5f3a033ae83e00a05
Achaudhari02 <ashutoshc018@gmail.com>
2023-11-28 15:44:58 -0500

    added junit tests

--- app/src/test/java/com/example/a2340c_team45/PlayerMoveTest.java
+++ app/src/test/java/com/example/a2340c_team45/PlayerMoveTest.java
@@ -3,6 +3,8 @@ package com.example.a2340c_team45;
 import static org.junit.Assert.assertEquals;
 
 import com.example.a2340c_team45.Strategy.MoveDown;
+import com.example.a2340c_team45.Strategy.MoveLeft;
+import com.example.a2340c_team45.Strategy.MoveRight;
 import com.example.a2340c_team45.Strategy.MoveUp;
 import com.example.a2340c_team45.models.Player;
 
@@ -26,5 +28,24 @@ public class PlayerMoveTest {
         player.setMovementStrat(new MoveDown());
         player.move();
         assertEquals(player.getY(), 0);
+    }
+    @Test
+    public void testLeft() {
+        Player player = Player.getPlayer();
+        player.setX(10);
+        player.setY(10);
+        player.setMovementStrat(new MoveLeft());
+        player.move();
+        assertEquals(player.getX(), 0);
+    }
+
+    @Test
+    public void testRight() {
+        Player player = Player.getPlayer();
+        player.setX(0);
+        player.setY(10);
+        player.setMovementStrat(new MoveRight());
+        player.move();
+        assertEquals(player.getX(), 10);
     }
 }
